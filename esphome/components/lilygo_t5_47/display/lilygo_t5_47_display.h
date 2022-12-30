0a4c2f4f46bb92edc92be60c25ba60e8b4d604b4
Danny81 <danny_1981@hotmail.it>
2022-12-30 23:47:53 +0100

    test draw absolute pixel

--- esphome/components/lilygo_t5_47/display/lilygo_t5_47_display.h
+++ esphome/components/lilygo_t5_47/display/lilygo_t5_47_display.h
@@ -45,9 +45,10 @@ class LilygoT547Display : public PollingComponent, public display::DisplayBuffer
 #if ESPHOME_VERSION_CODE >= VERSION_CODE(2022,6,0)
   display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_BINARY; }
 #endif
-
- protected:
-  void HOT draw_absolute_pixel_internal(int x, int y, Color color) override;
+  
+void HOT draw_absolute_pixel_internal(int x, int y, Color color) override;
+ 
+  protected:
 
  private:
   uint32_t full_update_every_;
